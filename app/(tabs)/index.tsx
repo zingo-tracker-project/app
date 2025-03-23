92e614fdde376076c0c7e9f86e9e0720a27fa169
bigboyang <bigboyang@naver.com>
2025-03-23 19:45:39 +0900

    feat: kakao logout

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,13 +3,10 @@ import { View, ActivityIndicator, Button, Text, Image } from 'react-native';
 import { WebView } from 'react-native-webview';
 import { useKakaoLogin } from '../../hooks/useKakaoLogin';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import * as AuthSession from 'expo-auth-session';
 import { useIsFocused, useNavigation } from '@react-navigation/native';
 
 export default function HomeScreen() {
   const [userInfo, setUserInfo] = useState<{ nickname: string; profileImage: string; id: string } | null>(null);
-  
-  // TODO navitage로 동작하도록 변경 필요
   const isFocused = useIsFocused();
 
   useEffect(() => {
@@ -25,21 +22,41 @@ export default function HomeScreen() {
       }
     };
     fetchUserInfo();
-  }, []);
+  }, [isFocused,userInfo]);
 
-  const { webViewRef, setLoading, loading, showWebView, setShowWebView, handleNavigationStateChange, KAKAO_AUTH_URL, handleLogout } =
+  const { webViewRef, setLoading, loading, showWebView, setShowWebView, handleNavigationStateChange, KAKAO_AUTH_URL, handleLogout, handleKakaoLogout, handleTokenRefresh, checkAccessTokenValidity } =
     useKakaoLogin(
-      (userData) => {
+      async (userData) => {
         setUserInfo(userData);
       },
       () => {
         setUserInfo(null);
-        !isFocused
       }
     );
 
+  const handleCheckTokenValidity = async () => {
+    try {
+      const accessToken = await AsyncStorage.getItem('kakao_token');
+      if (accessToken) {
+        const isValid = await checkAccessTokenValidity(accessToken);
+        if (!isValid) {
+          console.log('invalid token, token refresh start...');
+          const refreshedToken = await handleTokenRefresh();
+          if (refreshedToken) {
+            console.log('token refreshed');
+          }
+        }
+      }
+    } catch (error) {
+      console.log('token err :', error);
+    }
+  };
+
+  useEffect(() => {
+    handleCheckTokenValidity();
+  }, [isFocused]);
+
   return (
-    
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff' }}>
       {!showWebView ? (
         <>
@@ -47,7 +64,7 @@ export default function HomeScreen() {
             <View style={{ alignItems: 'center' }}>
               <Image source={{ uri: userInfo.profileImage }} style={{ width: 100, height: 100, borderRadius: 50, marginBottom: 10 }} />
               <Text style={{ fontSize: 20, fontWeight: 'bold', color: '#000' }}>{userInfo.nickname}님, 환영합니다! 🎉</Text>
-              <Button title="로그아웃" color="red" onPress={handleLogout} />
+              <Button title="카카오 로그아웃" color="red" onPress={handleLogout} />
             </View>
           ) : (
             <Button title="카카오 로그인" color="yellow" onPress={() => setShowWebView(true)} />
